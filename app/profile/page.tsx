03f20774a26b3c3591fd6d803ca6cfd0b49737e2
Sonny Bui <sbui@mediasolutionscorp.com>
2024-03-11 14:50:04 -0700

    update header for profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -10,15 +10,14 @@ export default async function Profile() {
   const user = await getUser();
 
   return (
-    <div className="flex min-h-screen items-center justify-center p-24">
-      <div className="flex flex-col items-center justify-center">
-        <div className="min-w-full">
-            <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-xl mb-10">     
-                <User user={user}/>
-            </div>
-        </div>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-xl mb-2">     
+          <div className="p-5">
+            <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">My Profile</h5>
+            <User user={user} />
+          </div>
       </div>
-    </div>
+  </div>
   );
 }
 
